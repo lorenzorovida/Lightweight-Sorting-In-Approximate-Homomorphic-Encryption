ea79832ef98fa8fdb6a71800e9e7852602c7f793
narger <lorenzo.rovida@outlook.it>
2026-01-26 16:16:57 +0100

    Prepating network experiments

--- experiments/network-based/random-0.001.sh
+++ experiments/network-based/random-0.001.sh
@@ -15,7 +15,7 @@ for INPUTS in 16 32 64 128 256 512 1024 2048 4096 8192; do
     echo " Run with inputs: $INPUTS" | tee -a "$RESULTS_FILE"
     echo "----------------------------------------" | tee -a "$RESULTS_FILE"
 
-    output=$(/usr/bin/time -l ../../build/Sort --random "$INPUTS" --delta 0.001 --network 2> >(grep "peak memory footprint"))
+    output=$(/usr/bin/time -l ../../build/Sort --random "$INPUTS" --delta 0.001 --network --toy2> >(grep "peak memory footprint"))
 
     echo "$output, inputs: $INPUTS" | tee -a "$RESULTS_FILE"
     echo "" | tee -a "$RESULTS_FILE"
